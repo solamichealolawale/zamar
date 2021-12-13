2586b565ad75478db595df517f81c3fbd37517da
Olusola Olawale <solamichealolawale@gmail.com>
2021-12-13 07:28:58 +0100

    Initial Commit
    Signed-off-by: Olusola Olawale <solamichealolawale@gmail.com>

---
+++ index.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- set the encoding of your site -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- set the page title -->
+	<title>Zamar@1</title>
+
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="apple-mobile-web-app-title" content="Zamar@1">
+	<meta name="application-name" content="Zamar@1">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="theme-color" content="#ffffff">
+
+
+	<link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&amp;family=Lora:ital,wght@0,400;0,700;1,400;1,700&amp;family=Muli:ital@0;1&amp;family=Merriweather&amp;display=swap" rel="stylesheet">
+	<!-- include the site bootstrap stylesheet -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- include the site stylesheet -->
+	<link rel="stylesheet" href="style.css">
+	<!-- include the site responsive stylesheet -->
+	<link rel="stylesheet" href="css/responsive.css">
+</head>
+
+<body class="hClassVvi">
+	<!-- pageWrapper -->
+	<div id="pageWrapper">
+		<div class="phStickyWrap w-100">
+			<!-- pageHeader -->
+			<header id="pageHeader" class="position-absolute w-100">
+				<div class="hdHolder headerFixer py-2 py-lg-3">
+					<div class="container">
+						<div class="row align-items-center">
+							<div class="col-5">
+								<!-- logo -->
+								<div class="logo text-white h2 small">
+									<a href="/">
+										Zamar@1
+									</a>
+								</div>
+							</div>
+							<div class="col-7 position-static d-flex justify-content-end">
+								<a href="javascript:void(0);" class="hdMenuOpener hdSideMenuOpener position-relative ml-4 d-block">
+									<span class="icnBar position-absolute"><span class="sr-only">menu</span></span>
+								</a>
+								<nav class="navbar navbar-light d-flex flex-column navbarSide position-fixed h-100 px-6 pt-10 pb-6 px-md-10 pb-md-10">
+									<ul class="navbar-nav mainNavigation w-100 flex-grow-1 mainSideNavigation text-capitalize d-block">
+										<li class="nav-item dropdown">
+											<a class="nav-link mt-3" href="#letters">Letter From Mum & Dad</a>
+											<a class="nav-link mt-3" href="#photos">Gallery</a>
+											<a class="nav-link btn btn-outline-warning py-2 rounded text-white mt-3" href="#bless">Bless our Son</a>
+										</li>
+									</ul>
+									<a href="javascript:void(0);" class="hdMenuOpener hdSideMenuOpener position-absolute">
+										<span class="icnBar position-absolute"><span class="sr-only">menu</span></span>
+									</a>
+								</nav>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+		</div>
+		<main>
+			<!-- bsSlidViv -->
+			<div class="bsSlidViv">
+				<div>
+					<!-- introBannerBlock -->
+					<article class="introBannerBlock introBannerBlockViv hdIsVix w-100 text-center text-white position-relative d-flex bgCover" style="background-image: url(images/zamar/33.jpeg);">
+						<div class="ibbAlignHolder w-100 d-flex align-items-center">
+							<div class="ibbHolder w-100 py-6">
+								<div class="container holder position-relative">
+									<div class="row">
+										<div class="col-12 col-sm-10 offset-sm-1">
+											<strong class="d-block fontSerif mainHeadingTitle font-weight-normal mb-5">Celebrating
+												God's Love & Faithfulness</strong>
+											<h1 class="text-white ibbHeading ibbHeadingViv mb-4 mb-lg-8">Baby Zamar @
+												1 </h1>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</article>
+				</div>
+				<div>
+					<!-- introBannerBlock -->
+					<article class="introBannerBlock introBannerBlockViv hdIsVix w-100 text-center text-white position-relative d-flex bgCover" style="background-image: url(images/zamar/24.jpeg);">
+						<div class="ibbAlignHolder w-100 d-flex align-items-center">
+							<div class="ibbHolder w-100 py-6">
+								<div class="container holder position-relative">
+									<div class="row">
+										<div class="col-12 col-sm-10 offset-sm-1">
+											<strong class="d-block fontSerif mainHeadingTitle font-weight-normal mb-5">You
+												are one 'one-der-ful' one-year-old!</strong>
+											<h1 class="text-white ibbHeading ibbHeadingViv mb-4 mb-lg-8">Congratulations
+												Baby Zamar</h1>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</article>
+				</div>
+			</div>
+
+			<div id="letters"></div>
+			<div class="exhiWrap">
+				<!-- exhibitDescrColumn -->
+				<article id="mom" class="position-relative overflow-hidden d-flex w-100 exhibitDescrColumn bg-grayf8f8f8 py-6 py-md-0 mb-6 mb-md-0">
+					<div class="alignWrap d-flex align-items-center w-100">
+						<div class="container">
+							<div class="row">
+								<div class="col-12 col-md-6 position-static">
+									<div class="imgHolder mb-4 mb-md-0 bgCover" style="background-image: url(images/zamar/27.jpeg);">
+										<img src="images/zamar/10.jpeg" class="sr-only" alt="image description">
+									</div>
+								</div>
+								<div class="col-12 col-md-6">
+									<div class="extraWrap pl-md-3 pl-xl-9 mr-xlwd-n10 pl-xlwd-13 py-md-6">
+										<h1 class="mb-4">Letter from Mom</h1>
+										<div class="eabDescrText eabDescrTextV text-gray888">
+
+											<p class="mb-5">
+												Wow. I won't lie, I can’t believe how quickly the past year has gone -
+												your first
+												birthday! I know you
+												don’t understand what that means, and probably won’t for another year or
+												two. That’s okay, it is special anyway. It is not only the celebration
+												of the first year of your life, but also the anniversary of your daddy
+												and I being parents. You have helped us grow in maturity, patience,
+												tolerance and, most importantly, our capacity to love. I can say without
+												hesitation that neither of us have ever felt anything even close to
+												the boundless love we have for you.
+											</p>
+
+											<p class="mb-5">I want you to know that nothing brings me more joy than
+												seeing your little face looking up at me when I wake in the morning, and
+												seeing your adorable profile as you sleep soundly in your cot at night.
+												If, sometimes, it seems like I am upset or stressed out, it is not your
+												fault. It is because, like you, I am learning new things, and need a bit
+												of time to understand what I need to do. Being your mother is my biggest
+												blessing and my proudest achievement.</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</article>
+				<!-- exhibitDescrColumn -->
+				<article id="dad" class="position-relative overflow-hidden d-flex w-100 exhibitDescrColumn bg-grayf8f8f8 py-6 py-md-0 mb-6 mb-md-0">
+					<div class="alignWrap d-flex align-items-center w-100">
+						<div class="container">
+							<div class="row">
+								<div class="col-12 col-md-6 position-static order-md-2">
+									<!-- bgHasParallax -->
+									<div class="imgHolder mb-4 mb-md-0 bgCover" style="background-image: url(images/zamar/29.jpeg);">
+										<img src="images/zamar/29.jpeg" class="sr-only" alt="image description">
+									</div>
+								</div>
+								<div class="col-12 col-md-6 order-1">
+									<div class="extraWrap pr-md-3 pr-xl-9 ml-xlwd-n10 pr-xlwd-13 py-md-6">
+										<h1 class="mb-4">Letter from Dad</h1>
+										<div class="eabDescrText eabDescrTextV text-gray777">
+											<p class="mb-5">I felt excited, when you clutched my fingers for the first
+												time ever
+												I smiled, because I realized that I finally became a father. We
+												laughed, when you growing up into a little boy.
+												Today we are so happy because you fill our lives with joy...</p>
+											<p class="mb-5">Right now, you are this tiny bundle of potential. I can
+												already see little buds of talent waiting to bloom as you grow and
+												absorb the world around you.
+												Everything is a wonder to you, and you love examining how things
+												work, and how things fit together. You are such a clever little man –
+												you quickly work out how to do things (and, sometimes, work out your own
+												way of doing them!). I hope you never lose that inquisitive nature of
+												yours, as it will serve you well throughout life. Learning is a gift,
+												because it expands the possibilities available to you in all areas.
+											</p>
+											<p class="mb-5"></p>
+											I hope that you continue to grow and enjoy
+											God's love, just as you are doing
+											now.
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</article>
+				<!-- exhibitDescrColumn -->
+			</div>
+			</section>
+
+
+			<section id="photos" class="exploreBlock bg-secondary pt-6 pb-3 pt-md-12 pb-md-6 pt-lg-17 pb-lg-12 pt-xl-22 pb-xl-17">
+				<div class="container">
+					<div class="row">
+						<div class="col-12 col-md-4 pr-lg-10 pr-xl-16 stickyContainer">
+							<!-- headHeading -->
+							<header class="headHeading hhFzMedium text-gray888 pt-2 mb-6 stickyElem sceHeadStick">
+								<h1 class="text-white mb-5">A picture is worth a thousand words</h1>
+								<p class="mb-3 mb-lg-6 mb-xl-10">It's amazing how big of a blessing such a small person can be. Thank you for being an amazing one-year-old!
+								</p>
+							</header>
+						</div>
+						<div class="col-12 col-md-8">
+							<div class="row isoContentHolder">
+								<?php
+								for ($x = 1; $x <= 33; $x += 1) { ?>
+									<div class="col-12 col-sm-6 col-lg-4 isoCol <?php echo $x <= 18 ? 'd-none d-md-block' : '' ?>">
+										<figure class="galFig galFigII position-relative mx-auto mx-md-0 mb-6">
+											<a data-fancybox="gallery" href="/images/zamar/<?php echo $x ?>.jpeg">
+												<img src="/images/zamar/<?php echo $x ?>.jpeg" class="img-fluid" alt="image description">
+											</a>
+											<div class="gafCaption inaccessible d-flex align-items-center justify-content-center text-center text-white w-100 h-100">
+												<div class="wrap w-100 p-4">
+													<i class="rounded-circle icnWrap icomoon-search bg-brown d-flex align-items-center justify-content-center mx-auto mb-2"><span class="sr-only">icon</span></i>
+												</div>
+											</div>
+										</figure>
+									</div>
+								<?php } ?>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+
+			<aside id="bless" class="timetableAside bgCover bgHasParallax position-relative pt-5 pb-5 pt-md-9 pb-md-9 pt-lg-12 pb-lg-13 pt-xl-22 pb-xl-22" style="background-image: url(images/zamar/6.jpeg);">
+				<div class="container">
+					<!-- ttaHolderBox -->
+					<div class="ttaHolderBox position-relative rounded-lg bg-white mx-auto text-center px-6 pt-6 pb-6 px-lg-10 pt-lg-9 pb-lg-10">
+						<h3 class="mb-4">Help us bless our son on his first birthday & make his first year a memorable
+							one</h3>
+						<p class="mb-7 headingHead text-gray888 text-uppercase small">We appreciate your love, presence
+							and
+							support. If you want to help us make it
+							unforgettable, you can contribute using the link below.</p>
+						<a href="https://pay.glade.ng/zamar" class="btn btn-success btnMidMinWidth">Bless the child</a>
+					</div>
+				</div>
+			</aside>
+
+			<section class="introBannerBlock introBannerBlockVv w-100 text-center position-relative d-flex text-gray888 bgParallax" style="background-image: url(images/pattern01.jpg);">
+				<div class="ibbAlignHolder w-100 d-flex align-items-center">
+					<div class="ibbHolder w-100 pt-22 pb-6">
+						<div class="container holder position-relative">
+							<div class="row">
+								<div class="col-12">
+									<strong class="d-block fontSerif mainHeadingTitle mainHeadingTitleII font-weight-normal text-secondary mb-2">Celebrating
+										God's Love & Faithfulness</strong>
+									<h1 class="ibbHeading ibbHeadingIII position-relative ibbHeadingHasMark d-inline-block align-top mb-4 mb-md-9 px-6 px-xl-9">
+										Zamar Abioye @ 1</h1>
+									<div class="eabDescrText col-lg-8 mx-auto">
+										<p class="mb-7">We pray that you know your Lord a least as well as your parents and grand parents do. we pray that you grow up strong in His Word, committed to His will and blessed in His grace and mercy.
+											God be with you, King Zamar. You are the living, breathing, laughing, cooing evidence that He is with us.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		</main>
+		<!-- ftAreaWrap -->
+		<div class="ftAreaWrap text-gray888">
+			<!-- pageFooter -->
+			<footer id="pageFooter" class="text-gray777 border-top text-center pt-6 pb-3 pt-lg-11 pb-lg-9">
+				<div class="container">
+					<p class="mb-2">Handcrafted with 💖 from <a class="text-underline" href="https://solar.disha.page">Uncle Solar</a> - © 2021.
+						All
+						Rights Reserved</p>
+				</div>
+			</footer>
+		</div>
+	</div>
+	<!-- include jQuery library -->
+	<script src="js/jquery-3.4.1.min.js"></script>
+	<!-- include bootstrap popper JavaScript -->
+	<script src="js/popper.min.js"></script>
+	<!-- include bootstrap JavaScript -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- include custom JavaScript -->
+	<script src="js/jqueryCustom.js"></script>
+	<!-- include fontawesome kit -->
+	<!-- <script src="../../../kit.fontawesome.com/391f644c42.js"></script> -->
+</body>
+
+</html>
